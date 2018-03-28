d43d7c06459652cde5b94f4229db1c4c753ada7d
Frank Rose <frankrosework@gmail.com>
2018-03-28 12:05:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var shout = function(string) {
+  return string.toUpperCase();
+}
+
+var whisper = function(string) {
+  return string.toLowerCase();
+}
+
+var logShout = function(string) {
+  console.log(string.toUpperCase());
+}
+
+var logWhisper = function(string) {
+  console.log(string.toLowerCase());
+}
